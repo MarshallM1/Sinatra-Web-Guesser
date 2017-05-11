27f36d2cdf7177b602b938cc21200664438de491
MarshallM1 <tebag9084@gmail.com>
2017-05-10 17:30:34 -0700

    Completed number guesser

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,8 +1,35 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-number = rand(100)
+set :secret_number, rand(101)
+set :guesses, 8
 
 get '/' do
-	erb :index, :locals => {:number => number}
+	guess = params['guess']
+	if settings.guesses == 0
+		settings.guesses = 8
+		settings.secret_number = rand(101)
+		message = "You have run out of guesses, please try again"
+	else
+		settings.guesses -= 1
+		message = check_number(guess)
+	end
+	erb :index, :locals => {:number => settings.secret_number, :message => message, :guess => guess}
+end
+
+def check_number(guess)
+	if guess.nil?
+		return message = "Guess the number (0-100)"
+	end
+	diff = settings.secret_number - guess.to_i
+	if diff < 0
+		message = "Your guess of #{guess} is too high"
+	elsif diff > 0
+		message = "Your guess of #{guess} is too low"
+	else
+		settings.secret_number = rand(101)
+		settings.guesses = 8
+		message = "You have guessed correctly, the secret number was #{guess}"
+	end
+		
 end
